95f738d9f2a6470215b84f329ff8d15cc2444e70
Victor Chen <victor_chen94@berkeley.edu>
2016-02-20 15:29:33 -0800

    added animation to visualization

--- victor_work/loader.py
+++ victor_work/loader.py
@@ -1,16 +1,20 @@
 import json
 import matplotlib.pyplot as plt
+import time
 
 from pprint import pprint
 
 
 def main():
-    with open('sample.json') as data_file:
+    with open('110.json') as data_file:
         data = json.load(data_file)
 
     teamData = ['homeTrackingData', 'awayTrackingData']
 
     plt.figure()
+    plt.ion()
+
+    teamLocations = []
 
     for i in range(2):
         team = teamData[i]
@@ -28,25 +32,30 @@ def main():
                 playerLocation.append((locationData['x'], locationData['y']))
             playerLocations.append(playerLocation)
 
+        teamLocations.append(playerLocations)
 
-        # plotting 1 player
-        xData = []
-        yData = []
+    index = 0
+    for time in range(len(teamLocations[i][0])):
+        for teamInd in range(2):
+            for playerInd in range(len(teamLocations[i])):
+                x,y = teamLocations[teamInd][playerInd][time]
+                color = 'b' if teamInd == 0 else 'r'
+                plt.scatter(x, y, c=color, s=100)
+
+        xmin = 10
+        xmax = 110
+        ymin = 0
+        ymax = 53.3
+        axes = plt.gca()
+        axes.set_xlim([xmin,xmax])
+        axes.set_ylim([ymin,ymax])
+
+        plt.draw()
+        plt.pause(0.01)
+        plt.clf()
 
 
-        for player in playerLocations:
-            xData = []
-            yData = []
+    return
 
-            for x,y in player:
-                xData.append(x)
-                yData.append(y)
-
-            if i == 0:
-                plt.plot(xData, yData, c='r')
-            else:
-                plt.plot(xData, yData, c='b')
-
-    plt.show()
 
 main()
