90ecbab15395fb49e02b1671371bd8293da226d2
Victor Chen <victor_chen94@berkeley.edu>
2016-02-20 11:40:52 -0800

    added away team graph

--- victor_work/loader.py
+++ victor_work/loader.py
@@ -8,36 +8,44 @@ def main():
     with open('sample.json') as data_file:
         data = json.load(data_file)
 
-    playerLocationData = data['homeTrackingData']
-
-    # print playerLocationData
-
-    playerLocations = []
-
-    for playerData in playerLocationData:
-        trackingData = playerData['playerTrackingData']
-        playerLocation = []
-
-        for locationData in trackingData:
-            playerLocation.append((locationData['x'], locationData['y']))
-        playerLocations.append(playerLocation)
-
-
-    # plotting 1 player
-    xData = []
-    yData = []
+    teamData = ['homeTrackingData', 'awayTrackingData']
 
     plt.figure()
 
-    for player in playerLocations:
+    for i in range(2):
+        team = teamData[i]
+        playerLocationData = data[team]
+
+        # print playerLocationData
+
+        playerLocations = []
+
+        for playerData in playerLocationData:
+            trackingData = playerData['playerTrackingData']
+            playerLocation = []
+
+            for locationData in trackingData:
+                playerLocation.append((locationData['x'], locationData['y']))
+            playerLocations.append(playerLocation)
+
+
+        # plotting 1 player
         xData = []
         yData = []
 
-        for x,y in player:
-            xData.append(x)
-            yData.append(y)
 
-        plt.plot(xData, yData)
+        for player in playerLocations:
+            xData = []
+            yData = []
+
+            for x,y in player:
+                xData.append(x)
+                yData.append(y)
+
+            if i == 0:
+                plt.plot(xData, yData, c='r')
+            else:
+                plt.plot(xData, yData, c='b')
 
     plt.show()
 
